906fc8b6a613cdb5a1faef433a002ec04615e6b0
Marina <errorda@gmail.com>
2022-11-17 19:39:58 +0300

    added task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿int dayNum = ReadInt("Введите число дня недели:");
+if ((dayNum < 1) || (dayNum > 7))
+
+    Console.WriteLine("Введите число от 1 до 7");
+
+else
+{
+
+if (dayNum<=5)
+
+Console.WriteLine("Рабочий день");
+
+else
+
+Console.WriteLine("Выходной");
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
