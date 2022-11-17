6b17dbc9b9f82d9c569a13f23d12dbf822ef70bd
Marina <errorda@gmail.com>
2022-11-17 19:39:03 +0300

    added task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int num1 = ReadInt("Введите число:");
+int dig1 = num1/10;
+int dig2 = dig1%10;
+Console.WriteLine(dig2);
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
