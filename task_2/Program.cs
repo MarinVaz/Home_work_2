8e6542570ce2bf2ea1522a5d7d7630c1e06a8cc7
Marina <errorda@gmail.com>
2022-11-17 19:39:23 +0300

    added task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿int num1 = ReadInt("Введите число:");
+
+if (ThirdDigit(num1) == -1)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine($"Третье число: {ThirdDigit(num1)}");
+
+int ThirdDigit(int num1)
+    {
+        int result = -1;
+            if (num1 >= 100)
+            {
+                while (num1 > 999)
+                {
+                    num1 = num1 / 10;
+                }
+                result = num1 % 10;
+            }
+            return result; 
+    }
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
